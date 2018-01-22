7d498c6ff9bc5383868d320fb02aa1a3c3b0a007
Muratov Nizhat <Nizha.777@mail.ru>
2018-01-22 22:03:58 +0600

    first commit

---
+++ 2 задача.py
@@ -0,0 +1,6 @@
+x = input('Enter the first line:')
+y = input('Enter the second line:')
+if x>y:
+	print("first line")
+else: 
+	print("second line")
---
+++ 3 задача.py
@@ -0,0 +1,7 @@
+x = int(input('Enter the first number:'))
+y = int(input('Enter the second number:'))
+z = x%y
+if z==1:
+	print("Uvedomlenie!!!")
+else: 
+	print(z)
